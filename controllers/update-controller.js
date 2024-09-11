52060bcf0d2fc9b37d71a16c440cfe869fac73aa
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-09-11 18:12:27 +0500

    latest

--- controllers/update-controller.js
+++ controllers/update-controller.js
@@ -1,41 +1,22 @@
 const User = require("../models/register-model");
-const cloudinary = require("../config/cloudinary");
-const fs = require("fs");
 
 const updateController = async (req, res) => {
     try {
         const { id } = req.params;
         const { name, fatherName, rollNumber, grade, formBay } = req.body;
         
-        let updatedUserData = { name, fatherName, rollNumber, grade, formBay };
+         const updatedUser = await User.findByIdAndUpdate(id, { name, fatherName, rollNumber, grade, formBay });
 
-        if (req.file) {
-            const result = await cloudinary.uploader.upload(req.file.path, {
-                folder: 'studentsrecords',
-            });
-
-            updatedUserData.image = result.secure_url;
-
-            fs.unlinkSync(req.file.path);
-        }
-
-        const updatedUser = await User.findByIdAndUpdate(id, updatedUserData);
-
-        if (!updatedUser) {
+         if (!updatedUser) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        res.status(200).json({
-            message: "User updated successfully",
-            user: updatedUser,
+        res.status("200").json({
+            message: "User upadted successfully",
         });
     } catch (error) {
-        console.error("Something went wrong:", error);
-        res.status(500).json({
-            message: "Something went wrong",
-            error: error.message,
-        });
+        res.status("500").console.error("Sometrhing wrong");
     }
 };
 
-module.exports = updateController;
+module.exports =  updateController;
