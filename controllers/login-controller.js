2f92d9302c4e8af379e258c751bb471facbaa1ef
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-09-27 17:44:28 +0500

    latest

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -4,18 +4,15 @@ const loginController = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        // Check if the user exists
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        // Compare the plain-text password
         if (password !== user.password) {
             return res.status(401).json({ message: "Invalid password" });
         }
 
-        // If email and password are correct, return success response
         res.status(200).json({
             message: "Login successful",
             user: {
