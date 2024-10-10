1276a797f0d1921f0e697f5fe27f376018e70704
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-10-10 17:21:20 +0500

    latest

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://recordswebsite.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', "PATCH"],
 };
